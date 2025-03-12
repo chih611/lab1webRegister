1131d310c7bbd5ae0479fee898ce16b958381ebe
CHI THAI <21313683@students.ltu.edu.au>
2025-03-12 23:57:00 +1100

    fecth api

--- lab1/app/(tabs)/index.tsx
+++ lab1/app/(tabs)/index.tsx
@@ -1,74 +1,65 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { useState } from 'react';
+import { Image, StyleSheet, Platform, Button, View, TextInput, Text } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
+  const [text, onChangeText] = useState('https://reactnative.dev/movies.json');
+  const [webData, setWebData] = useState('');
+  const request = new XMLHttpRequest();
+
+  const _handlePressBtnAsync = () => {
+    request.onreadystatechange = e => {
+      if (request.readyState !== 4) {
+        return;
+      }
+      if (request.status === 200) {
+        console.log('test');
+        console.log('sucess, ' + request.responseText);
+        setWebData('Status: ' + request.status + ' ' + request.statusText + ' ' + request.responseText + ' ' + request.response);
+      } else {
+        console.warn('error');
+        setWebData('Error: ' + request.status + ' ' + request.statusText + ' ' + request.responseText + ' ' + request.response);
+      }
+    }
+    request.open('GET', text);
+    request.send();
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.containerColumn}>
+      <View style={styles.containerRow}>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          placeholder='Enter URL'
+          value={text} />
+        <Button
+          title='Click me'
+          onPress={() => _handlePressBtnAsync()} />
+      </View>
+      <Text style={styles.text}>{webData}</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  containerColumn: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    padding: 40
+  },
+  containerRow: {
     flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+    justifyContent: 'space-between'
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  input: {
+    justifyContent: 'center',
+    borderBottomColor: 'blue',
+    borderWidth: 1,
+    width: 200,
+    color: 'blue'
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+  text: {
+    color: 'blue'
+  }
 });
